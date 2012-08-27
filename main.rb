1ca6c3990e4aa79f7725daeaf964b0059d1c4351
Bryan Schlief <bschlief@gmail.com>
2012-08-26 18:09:12 -0700

    Convert to curses from ncurses.

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-require 'ffi-ncurses'
-include FFI::NCurses
+require 'curses'
+include Curses
 
 ############################################
 # displays the entries which contain
@@ -9,11 +9,11 @@ include FFI::NCurses
 # up to the number of rows provided by
 # the terminal
 def disp(search_str, col_headers, entries, selection_idx)
-  rows = 210
+  rows = lines
 
   # adjustments for search string,
   # 0 based index, column headers
-  rows -= 6
+  rows -= 4
 
   # mask the entries by the strings containing
   # all the sarch terms
@@ -25,11 +25,24 @@ def disp(search_str, col_headers, entries, selection_idx)
 
   addstr "Search? #{search_str}  (#{selected.length} Entries Selected) [Selected = #{selection_idx}]\n"
 
+  if selection_idx > selected.length then
+    selection_idx = selected.length  
+  end
+
   # print the column headers, and the 
   # selected items.  fill remainder of
   # terminal with whitespace. 
-  addstr col_headers + "\n"
-  selected[0..rows].each { |s| addstr s + "\n" }
+  addstr "#{col_headers}\n"
+  selected[0..rows].each_with_index do |str,idx|
+    if (idx == selection_idx)
+      attrset(A_BOLD)
+      attrset(A_REVERSE)
+    end
+    addstr "#{str}\n" 
+    if (idx == selection_idx)
+      attrset(A_NORMAL)
+    end
+  end
 
 end
 
@@ -63,43 +76,40 @@ def read_file(filename)
 end
 
 begin
-  initscr
+  init_screen
   raw
-  keypad stdscr, true
+  stdscr.keypad(true)
   noecho
-  curs_set 0
+  curs_set 2
   ch = 0
   name = "none"
 
   filename = "adp_csv.csv"
   entries = read_file(filename)
-
   col_headers = entries.shift
 
   search_str = ""
   selection_idx = 0
 
-  KEY_DELETE = 127
   KEY_ESCAPE = 27
 
-  ROW_MAX = 210
+  ROW_MAX = lines
   ROW_MIN = 0
 
   while ch != KEY_CTRL_Q
     clear
-#    addstr sprintf("name: %s dec: %d char: [%s]\n", name, ch, (1..127).include?(ch) ? ch.chr : " ")
 
-    if ((32..122).include?(ch)) then
+    if ch.class == String then
       search_str << ch
       selection_idx = 0 
-    end
-
+   end
+     
     if (ch == KEY_ESCAPE) then
       search_str = "" 
       selection_idx = 0   
     end 
 
-    if (ch == KEY_DELETE || ch == KEY_BACKSPACE) then
+    if (ch == KEY_DC || ch == KEY_DL || ch == KEY_BACKSPACE) then
       search_str.chop! 
       selection_idx = 0
     end
@@ -114,12 +124,13 @@ begin
       selection_idx = ROW_MIN if selection_idx < ROW_MIN
     end
 
+#    addstr "Ch is #{ch.class} #{ch.to_i} #{keyname(ch)} SEARCH: #{search_str}\n"
     disp(search_str, col_headers, entries, selection_idx)
     refresh
     ch = getch
-    name = keyname(ch)
+
   end
 ensure
-  endwin
+  close_screen 
 end
 
